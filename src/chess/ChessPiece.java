1a600150bc1ecb5f1827de157ec2ec7a6dbe43aa
LuizFernandoDeveloper <luiz.fernando.developer@outlook.com>
2023-03-24 16:35:34 -0300

    Class ChessPiece

---
+++ src/chess/ChessPiece.java
@@ -0,0 +1,18 @@
+package chess;
+
+import boardGame.Board;
+import boardGame.Piece;
+import chess.enums.Color;
+
+public class ChessPiece extends Piece {
+    private Color color;
+
+    public ChessPiece(Board board,Color color) {
+        super(board);
+        this.color = color;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+}
